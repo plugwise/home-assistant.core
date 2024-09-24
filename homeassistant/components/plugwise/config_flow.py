bbeb09d7576eee16496e5b64ebf6074b7bd9a5fb
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-09-24 19:12:26 +0200

    Improve config_flow

--- homeassistant/components/plugwise/config_flow.py
+++ homeassistant/components/plugwise/config_flow.py
@@ -16,8 +16,9 @@ from plugwise.exceptions import (
 import voluptuous as vol
 
 from homeassistant.components.zeroconf import ZeroconfServiceInfo
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import SOURCE_USER, ConfigFlow, ConfigFlowResult
 from homeassistant.const import (
+    ATTR_CONFIGURATION_URL,
     CONF_BASE,
     CONF_HOST,
     CONF_NAME,
@@ -29,13 +30,11 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
-    API,
     DEFAULT_PORT,
     DEFAULT_USERNAME,
     DOMAIN,
     FLOW_SMILE,
     FLOW_STRETCH,
-    PW_TYPE,
     SMILE,
     STRETCH,
     STRETCH_USERNAME,
@@ -43,28 +42,41 @@ from .const import (
 )
 
 
-def _base_gw_schema(discovery_info: ZeroconfServiceInfo | None) -> vol.Schema:
+def _base_schema(
+    discovery_info: ZeroconfServiceInfo | None,
+    user_input: dict[str, Any] | None,
+) -> vol.Schema:
     """Generate base schema for gateways."""
-    base_gw_schema = vol.Schema({vol.Required(CONF_PASSWORD): str})
-
     if not discovery_info:
-        base_gw_schema = base_gw_schema.extend(
+        if not user_input:
+            return vol.Schema(
+                {
+                    vol.Required(CONF_PASSWORD): str,
+                    vol.Required(CONF_HOST): str,
+                    vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+                    vol.Required(CONF_USERNAME, default=SMILE): vol.In(
+                        {SMILE: FLOW_SMILE, STRETCH: FLOW_STRETCH}
+                    ),
+                }
+            )
+        return vol.Schema(
             {
-                vol.Required(CONF_HOST): str,
-                vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
-                vol.Required(CONF_USERNAME, default=SMILE): vol.In(
+                vol.Required(CONF_PASSWORD, default=user_input[CONF_PASSWORD]): str,
+                vol.Required(CONF_HOST, default=user_input[CONF_HOST]): str,
+                vol.Optional(CONF_PORT, default=user_input[CONF_PORT]): int,
+                vol.Required(CONF_USERNAME, default=user_input[CONF_USERNAME]): vol.In(
                     {SMILE: FLOW_SMILE, STRETCH: FLOW_STRETCH}
                 ),
             }
         )
 
-    return base_gw_schema
+    return vol.Schema({vol.Required(CONF_PASSWORD): str})
 
 
-async def validate_gw_input(hass: HomeAssistant, data: dict[str, Any]) -> Smile:
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> Smile:
     """Validate whether the user input allows us to connect to the gateway.
 
-    Data has the keys from _base_gw_schema() with values provided by the user.
+    Data has the keys from _base_schema() with values provided by the user.
     """
     websession = async_get_clientsession(hass, verify_ssl=False)
     api = Smile(
@@ -97,7 +109,7 @@ class PlugwiseConfigFlow(ConfigFlow, domain=DOMAIN):
         unique_id = discovery_info.hostname.split(".")[0].split("-")[0]
         if config_entry := await self.async_set_unique_id(unique_id):
             try:
-                await validate_gw_input(
+                await validate_input(
                     self.hass,
                     {
                         CONF_HOST: discovery_info.host,
@@ -156,7 +168,7 @@ class PlugwiseConfigFlow(ConfigFlow, domain=DOMAIN):
                     CONF_PORT: discovery_info.port,
                     CONF_USERNAME: self._username,
                 },
-                "configuration_url": (
+                ATTR_CONFIGURATION_URL: (
                     f"http://{discovery_info.host}:{discovery_info.port}"
                 ),
                 "product": _product,
@@ -168,39 +180,50 @@ class PlugwiseConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step when using network/gateway setups."""
-        errors = {}
+        errors: dict[str, str] = {}
 
-        if user_input is not None:
-            if self.discovery_info:
-                user_input[CONF_HOST] = self.discovery_info.host
-                user_input[CONF_PORT] = self.discovery_info.port
-                user_input[CONF_USERNAME] = self._username
+        if not user_input:
+            return self.async_show_form(
+                step_id=SOURCE_USER,
+                data_schema=_base_schema(self.discovery_info, None),
+                errors=errors,
+            )
 
-            try:
-                api = await validate_gw_input(self.hass, user_input)
-            except ConnectionFailedError:
-                errors[CONF_BASE] = "cannot_connect"
-            except InvalidAuthentication:
-                errors[CONF_BASE] = "invalid_auth"
-            except InvalidSetupError:
-                errors[CONF_BASE] = "invalid_setup"
-            except (InvalidXMLError, ResponseError):
-                errors[CONF_BASE] = "response_error"
-            except UnsupportedDeviceError:
-                errors[CONF_BASE] = "unsupported"
-            except Exception:  # noqa: BLE001
-                errors[CONF_BASE] = "unknown"
-            else:
-                await self.async_set_unique_id(
-                    api.smile_hostname or api.gateway_id, raise_on_progress=False
-                )
-                self._abort_if_unique_id_configured()
+        if self.discovery_info:
+            user_input[CONF_HOST] = self.discovery_info.host
+            user_input[CONF_PORT] = self.discovery_info.port
+            user_input[CONF_USERNAME] = self._username
+        try:
+            api = await validate_input(self.hass, user_input)
+        except ConnectionFailedError:
+            errors[CONF_BASE] = "cannot_connect"
+        except InvalidAuthentication:
+            errors[CONF_BASE] = "invalid_auth"
+        except InvalidSetupError:
+            errors[CONF_BASE] = "invalid_setup"
+        except (InvalidXMLError, ResponseError):
+            errors[CONF_BASE] = "response_error"
+        except UnsupportedDeviceError:
+            errors[CONF_BASE] = "unsupported"
+        except Exception:  # noqa: BLE001
+            errors[CONF_BASE] = "unknown"
 
-                user_input[PW_TYPE] = API
-                return self.async_create_entry(title=api.smile_name, data=user_input)
+        if errors:
+            return self.async_show_form(
+                step_id=SOURCE_USER,
+                data_schema=_base_schema(None, user_input),
+                errors=errors,
+            )
+
+        await self.async_set_unique_id(
+            api.smile_hostname or api.gateway_id, raise_on_progress=False
+        )
+        self._abort_if_unique_id_configured()
+
+        return self.async_create_entry(title=api.smile_name, data=user_input)
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=_base_gw_schema(self.discovery_info),
+            step_id=SOURCE_USER,
+            data_schema=_base_schema(self.discovery_info),
             errors=errors,
         )
